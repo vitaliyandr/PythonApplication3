b7a9339b32c247e1ab81fa26e2843c1f76766975
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2022-10-29 18:06:03 +0300

    Add files via upload

---
+++ PythonApplication8.py
@@ -0,0 +1,5 @@
+print ("Life is what happens when you're busy making other plans. \n John Lennon")
+
+print ("Life is what happens \n when you are busy making other plans. \n John Lennon")
+
+print ("Life \n is what happens when you're busy making other plans. \n John Lennon")
